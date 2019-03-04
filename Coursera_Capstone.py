443992251bd68aa2e014188efa8658baf3fa9f07
CVALAD13 <48191285+CVALAD13@users.noreply.github.com>
2019-03-03 17:48:49 -0800

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# This notebook will be mostly used for the capstone project!
+
+# In[1]:
+
+import pandas as pd
+
+
+# In[2]:
+
+import numpy as np
+
+
+# In[4]:
+
+print ("Hello Capstone Project Course!")
+
